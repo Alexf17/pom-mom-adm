30aa5d552774fab3ab2a7a3c0529a18584b80432
Darymee <danieledaniele3005@gmail.com>
2023-03-20 23:25:13 +0100

    1

--- src/pages/RecipePage/RecipePage.styled.jsx
+++ src/pages/RecipePage/RecipePage.styled.jsx
@@ -2,22 +2,18 @@ import styled from '@emotion/styled';
 
 export const CardWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
 `;
 
 export const ImgWrapper = styled.div`
-  /* width: 500px; */
-  border-radius: 10px;
-  /* overflow: hidden; */
+  width: 600px;
   margin-right: 40px;
-  flex-grow: 1;
 `;
 
-export const InfoWrapper = styled.div`
-  flex-grow: 1;
-`;
+export const InfoWrapper = styled.div``;
 
 export const Description = styled.p`
-  display: inline-block;
+  display: block;
   text-align: center;
   font-family: 'Luckiest Guy', cursive;
   margin-bottom: 20px;
